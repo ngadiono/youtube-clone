821b344e743248e77f51a5c31078b3666818a377
ngadiono <ngadionocr@gmail.com>
2022-08-19 14:19:45 +0700

    adding videos api

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,6 +1,19 @@
 import express from 'express';
-import {} from '../controllers/video.js';
+import { addVideo, addView, trend, random, sub, getByTag, search } from '../controllers/video.js';
+import { verifyToken } from '../verifyToken.js';
 
 const router = express.Router();
+
+//create a video
+router.post('/', verifyToken, addVideo);
+router.put('/:id', verifyToken, addVideo);
+router.delete('/:id', verifyToken, addVideo);
+router.get('/find/:id', verifyToken, addVideo);
+router.put('/view/:id', addView);
+router.get('/trend', trend);
+router.get('/random', random);
+router.get('/sub', verifyToken, sub);
+router.get('/tags', getByTag);
+router.get('/search', search);
 
 export default router;
